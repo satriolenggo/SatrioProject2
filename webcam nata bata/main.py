0332da2cff4f8f53721c3a4608ac2e6f6b9455b5
satriolenggo <satriolenggo@gmail.com>
2026-03-19 15:09:41 +0700

    Enhance Flask app configuration with folders
    Refactor Flask app initialization to include custom template and static folders.

--- webcam nata bata/main.py
+++ webcam nata bata/main.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
-app = app  # <--- TAMBAHKAN BARIS INI
+app = Flask(__name__,
+    template_folder=os.path.join(os.path.dirname(__file__), 'templates'),
+    static_folder=os.path.join(os.path.dirname(__file__), 'static'))
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    # Bagian ini hanya jalan di laptopmu (PyCharm), Vercel akan mengabaikannya
     print("--- LEON SANDBOX RUNNING ---")
     app.run(debug=True, port=5000)
